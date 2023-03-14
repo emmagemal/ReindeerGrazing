90ee6fc403f831e0675c2fa80b1ae4c673a1bc61
emmagemal <emmagemal@gmail.com>
2023-03-14 19:08:46 +0100

    redid nmds plot with new results

--- Scripts/figures.R
+++ Scripts/figures.R
@@ -10,13 +10,12 @@ library(viridis)
 
 #### Thesis Plot Theme ---- 
 theme_thesis <- theme_bw() +
-  theme(panel.grid = element_blank(),
-        axis.title.x = 
-          element_text(margin = margin(t = 10, r = 0, b = 0, l = 0)),
-        axis.title.y = 
-          element_text(margin = margin(t = 0, r = 10, b = 0, l = 0))) +
-  theme(plot.margin = unit(c(0.5, 0.5, 0.5, 0.5), "cm"))
-
+                  theme(panel.grid = element_blank(),
+                        axis.title.x = 
+                          element_text(margin = margin(t = 10, r = 0, b = 0, l = 0)),
+                        axis.title.y = 
+                          element_text(margin = margin(t = 0, r = 10, b = 0, l = 0))) +
+                  theme(plot.margin = unit(c(0.5, 0.5, 0.5, 0.5), "cm"))
 
 
 #### Data Import ----
@@ -30,54 +29,56 @@ slope <- read.csv("Data/slope.csv")
 soildepth <- read.csv("Data/soildepth.csv")
 wetness <- read.csv("Data/wetness.csv")
 
-#### Editing import format ----
-# Main variables 
+#### Editing Import Format ----
+## Main variables 
 sites <- sites %>% 
-  mutate(date = as.Date(date, format = "%d/%m/%y"),
-         plot = as.character(plot),
-         plot_nr = as.character(plot_nr)) %>% 
-  mutate(aspect = case_when(grepl("S", site) ~ "S",
-                            grepl("N", site) ~ "N"),
-         grazing_cat = substr(site, 1, 1))   # adding aspect
+            mutate(date = as.Date(date, format = "%d/%m/%y"),
+                   plot = as.character(plot),
+                   plot_nr = as.character(plot_nr),
+                   site_nr = as.character(site_nr)) %>% 
+            mutate(aspect = case_when(grepl("S", site) ~ "S",
+                                      grepl("N", site) ~ "N"),
+                   grazing_cat = substr(site, 1, 1))   # adding aspect
 str(sites)            
 
 plots <- plots %>% 
-  mutate(date = as.Date(date, format = "%d/%m/%y"),
-         plot_nr = as.character(plot_nr),
-         site_nr = as.character(site_nr))
+            mutate(date = as.Date(date, format = "%d/%m/%y"),
+                   plot_nr = as.character(plot_nr),
+                   site_nr = as.character(site_nr))
 str(plots)
 
 coverage <- coverage %>%  
-  mutate(date = as.Date(date, format = "%d/%m/%y"),
-         plot_rep = as.character(plot_rep),
-         plot_nr = as.character(plot_nr),
-         site_nr = as.character(site_nr)) 
+              mutate(date = as.Date(date, format = "%d/%m/%y"),
+                     plot_rep = as.character(plot_rep),
+                     plot_nr = as.character(plot_nr),
+                     site_nr = as.character(site_nr)) 
 print(coverage %>% group_by(plot_nr) %>% summarize(sum(coverage_perc)), n = 105)  # all att 100%
 str(coverage) 
 
-# GIS variables
+## GIS variables
 gisvar <- left_join(ndvi, slope)
 gisvar <- left_join(gisvar, wetness)
 gisvar <- left_join(gisvar, soildepth)
 
 str(gisvar)
 gisvar <- gisvar %>% 
-  mutate(plot_nr = as.character(plot_nr))
+            mutate(plot_nr = as.character(plot_nr))
 str(gisvar)
 
 
 ### Checking species 
 sort(unique(plots$sp_common))  # remove whitespace after words 
-# "purple mountain heather" & "purple mountain-heath" 
+                               # "purple mountain heather" & "purple mountain-heath" 
 
 sort(unique(plots$sp_latin))  # some don't have a latin name... not good 
-# remove whitespace after words
-# Euphrasia stricta var. tenuis & Euphrasia stricta var. tenius
-# "Peltigera aphthosa" & "Peltigera apthosa" 
-# "Pleurozium schereberi" & "Pleurozium schreberi"
-# "Stereocaulon alpinus" & "Stereocaulon alpinum"
-# "Stereocaulon sp," & "Stereocaulon sp."
-# "Vaxinium uliginosum" & "Vaccinium uliginosum"
+                              # remove whitespace after words
+                              # Euphrasia stricta var. tenuis & Euphrasia stricta var. tenius
+                              # "Peltigera aphthosa" & "Peltigera apthosa" 
+                              # "Pleurozium schereberi" & "Pleurozium schreberi"
+                              # "Stereocaulon alpinus" & "Stereocaulon alpinum"
+                              # "Stereocaulon sp," & "Stereocaulon sp."
+                              # "Vaxinium uliginosum" & "Vaccinium uliginosum"
+                              # "Pilocella sp." & "Pilocello sp." (should be Pilosella)
 
 unique(coverage$sp_group)  # empty row is seen as own character 
 coverage <- coverage %>% na.omit()   # remove empty row
@@ -89,60 +90,69 @@ unique(plots$sp_group)
 
 ### Editing species that had mistakes 
 plots <- plots %>% 
-  mutate(sp_common = ifelse(sp_common == "purple mountain-heath", 
-                            "purple mountain heather",
-                            sp_common)) %>% 
-  mutate(sp_common = str_trim(sp_common, side = "right")) %>% 
-  mutate(sp_latin = str_trim(sp_latin, side = "right")) %>% 
-  mutate(sp_latin = ifelse(sp_latin == "Peltigera apthosa", "Peltigera aphthosa",
-                           sp_latin),
-         sp_latin = ifelse(sp_latin == "Euphrasia stricta var. tenius", 
-                           "Euphrasia stricta var. tenuis", sp_latin),
-         sp_latin = ifelse(sp_latin == "Pleurozium schereberi", "Pleurozium schreberi",
-                           sp_latin),
-         sp_latin = ifelse(sp_latin == "Stereocaulon alpinus", "Stereocaulon alpinum",
-                           sp_latin),
-         sp_latin = ifelse(sp_latin == "Stereocaulon sp,", "Stereocaulon sp.",
-                           sp_latin),
-         sp_latin = ifelse(sp_latin == "Vaxinium uliginosum", "Vaccinium uliginosum",
-                           sp_latin))
+            mutate(sp_common = ifelse(sp_common == "purple mountain-heath", 
+                                      "purple mountain heather",
+                                      sp_common)) %>% 
+            mutate(sp_common = str_trim(sp_common, side = "right")) %>% 
+            mutate(sp_latin = str_trim(sp_latin, side = "right")) %>% 
+            mutate(sp_latin = ifelse(sp_latin == "Peltigera apthosa", "Peltigera aphthosa",
+                                     sp_latin),
+                   sp_latin = ifelse(sp_latin == "Euphrasia stricta var. tenius", 
+                                     "Euphrasia stricta var. tenuis", sp_latin),
+                   sp_latin = ifelse(sp_latin == "Pleurozium schereberi", "Pleurozium schreberi",
+                                     sp_latin),
+                   sp_latin = ifelse(sp_latin == "Stereocaulon alpinus", "Stereocaulon alpinum",
+                                     sp_latin),
+                   sp_latin = ifelse(sp_latin == "Stereocaulon sp,", "Stereocaulon sp.",
+                                     sp_latin),
+                   sp_latin = ifelse(sp_latin == "Vaxinium uliginosum", "Vaccinium uliginosum",
+                                     sp_latin),
+                   sp_latin = ifelse(sp_latin == "Pilocello sp.", "Pilosella sp.", sp_latin),
+                   sp_latin = ifelse(sp_latin == "Pilocella sp.", "Pilosella sp.", sp_latin))
 
 plots[(plots$sp_latin == ""), ]   # no latin name because they are "unknown"
 plots <- plots %>%  
-  mutate(sp_latin = ifelse(sp_latin == "", "unknown", sp_latin))
-
-
-
+            mutate(sp_latin = ifelse(sp_latin == "", "unknown", sp_latin))
 
 
 #### Variable Calculations ----
-### Species richness
+## Species richness
 plots <- plots %>% 
-  group_by(plot_nr) %>% 
-  mutate(richness_plot = length(unique(sp_latin))) %>%   # overall per plot
-  ungroup() %>% 
-  group_by(site_nr) %>% 
-  mutate(richness_site = length(unique(sp_latin))) %>%   # overall per site (no reps)
-  ungroup() %>% 
-  group_by(plot_nr, sp_group) %>% 
-  mutate(richness_group = length(unique(sp_latin))) %>%   # per group per plot 
-  ungroup()
+            group_by(plot_nr) %>% 
+            mutate(richness_plot = length(unique(sp_latin))) %>%   # overall per plot
+            ungroup() %>% 
+            group_by(site_nr) %>% 
+            mutate(richness_site = length(unique(sp_latin))) %>%   # overall per site (no reps)
+            ungroup() %>% 
+            group_by(plot_nr, sp_group) %>% 
+            mutate(richness_group = length(unique(sp_latin))) %>%   # per group per plot 
+            ungroup()
 
 richness <- plots %>% 
-  group_by(plot_nr, site_nr, sp_group) %>% 
-  summarize(richness_plot = mean(richness_plot),
-            richness_site = mean(richness_site),
-            richness_group = mean(richness_group),
-            height_site = mean(avg_height)) %>% 
-  ungroup() %>% 
-  group_by(plot_nr) %>% 
-  mutate(rich_group_prop = (richness_group/sum(richness_group))) %>% 
-  ungroup() %>%
-  group_by(site_nr) %>% 
-  mutate(richness_siteT = mean(richness_plot)) %>%  # true richness per site (with reps)
-  ungroup() %>% 
-  group_by(sp_group, site_nr) %>% 
-  mutate(rich_propT = mean(rich_group_prop))
+              group_by(plot_nr, site_nr, sp_group) %>% 
+              summarize(richness_plot = mean(richness_plot),
+                        richness_site = mean(richness_site),
+                        richness_group = mean(richness_group),
+                        height_site = mean(avg_height)) %>% 
+              ungroup() %>% 
+              group_by(plot_nr) %>% 
+              mutate(rich_group_prop = (richness_group/sum(richness_group))) %>% 
+              ungroup() %>%
+              group_by(site_nr) %>% 
+              mutate(richness_siteT = mean(richness_plot)) %>%  # true richness per site (with reps)
+              ungroup() %>% 
+              group_by(sp_group, site_nr) %>% 
+              mutate(rich_propT = mean(rich_group_prop))
+
+## GIS Standardization
+gisvar <- gisvar %>% 
+            mutate(across(c("ndvi":"soil_depth"), 
+                          .fns = list(s = scale))) %>% 
+            mutate(ndvi_s = as.vector(ndvi_s),
+                   slope_deg_s = as.vector(slope_deg_s),
+                   wetness_s = as.vector(wetness_s),
+                   soil_depth_s = as.vector(soil_depth_s))
+
 
 #### Combining dataframes ----
 ## Combining 'richness' with 'sites' to add grazing data 
@@ -392,7 +402,9 @@ veganCovEllipse <- function (cov, center = c(0, 0), scale = 1, npoints = 100)
   t(center + scale * t(Circle %*% chol(cov)))
 }  # run from here
 
+plot(nmds6)
 ord <- ordiellipse(nmds6, data.scores$aspect, label = T, conf = 0.95)
+dev.off()
 
 df_ell <- data.frame()   # run from here (this side)
 
@@ -412,22 +424,22 @@ gisvar2 <- gisvar %>%
 
 # making environmental variable vectors for the site 
 gisvar_long <- left_join(gisvar2, sites)  
-gisvar_long <- gisvar_long %>% mutate(grazing_m = grazing_s/60)
 
 gisvar_sites <- gisvar_long %>% 
-                  dplyr::select(site_nr, ndvi, slope_deg, wetness, soil_depth, grazing_s) %>% 
+                  dplyr::select(site_nr, ndvi, slope_deg, wetness, soil_depth) %>% 
                   group_by(site_nr) %>% 
                   summarize(ndvi = mean(ndvi),
                             slope_deg = mean(slope_deg),
                             soil_depth = mean(soil_depth),
-                            wetness = mean(wetness),
-                            grazing_m = mean(grazing_m)) %>% 
+                            wetness = mean(wetness)) %>% 
                   ungroup() %>% 
                   dplyr::select(!site_nr)
 
 gisfit <- envfit(nmds6, gisvar_sites, permu = 999)
+gisfit
 
-# gisarrows <- as.data.frame(scores(gisfit, "vectors"))    # use if significant env var 
+gisarrows <- as.data.frame(scores(gisfit, "vectors")) * ordiArrowMul(gisfit)
+  # use if significant environmental variables 
 
 
 ## NMDS Plot
@@ -439,6 +451,11 @@ gisfit <- envfit(nmds6, gisvar_sites, permu = 999)
                            size = 2, alpha = 0.6) +
                 #   geom_text(data = species_scores, aes(x = NMDS1, y = NMDS2, label = species),
                 #            alpha = 0.5, size = 2) +
+                geom_segment(aes(x = 0, y = 0, xend = NMDS1, yend = NMDS2), 
+                             data = gisarrows, size = 1, alpha = 0.7, colour = "grey40",
+                             arrow = arrow(length = unit(0.25, "cm"))) +
+                geom_text(data = gisarrows, aes(x = NMDS1, y = NMDS2), colour = "grey40", 
+                          label = row.names(gisarrows)) + 
                 theme_thesis + 
                 labs(x = "NMDS1", y = "NMDS2") +
                 scale_color_manual(values = c("#458264", "#F4A460"),
@@ -547,7 +564,9 @@ NMDSplot_mean <- aggregate(NMDSplot[,1:2], list(group = NMDSplot$group), "mean")
 
 
 ## Making the ellipsoids
+plot(nmds7)
 ord2 <- ordiellipse(nmds7, data.scoresb$aspect, label = T, conf = 0.95)
+dev.off()
 
 df_ell2 <- data.frame()   # run from here (this side)
 
@@ -561,21 +580,31 @@ for(g in levels(NMDSplot$group)){
 
 
 ## GIS variables 
-# removing unnecessary info from GIS data 
-gisvar_plots <- gisvar_long %>%  
-                  dplyr::select(plot_nr, ndvi, slope_deg, wetness, soil_depth, grazing_m) 
+# not standardized 
+gisvar_plots <- gisvar_long %>% dplyr::select(ndvi, slope_deg, wetness, soil_depth) 
 
-gisfit2 <- envfit(nmds7, gisvar_plots, permu = 999)
-gisfit2 
+gisfit3 <- envfit(nmds7, gisvar_plots, permu = 999)
+gisfit3 
 
-gisarrows2 <- as.data.frame(scores(gisfit2, "vectors") * ordiArrowMul(gisfit2))
+# standardized 
+gisvar_plots2 <- gisvar_long %>% dplyr::select(ndvi_s, slope_deg_s, wetness_s, soil_depth_s)
 
-# removing non-significant variables 
-remove <- c("soil_depth", "grazing_m")
-gisarrows2 <- gisarrows2[!(row.names(gisarrows2) %in% remove),]
+gisfit4 <- envfit(nmds7, gisvar_plots2, permu = 999)
+gisfit4
+
+gisarrows2 <- as.data.frame(scores(gisfit4, "vectors") * ordiArrowMul(gisfit4))
 
 
 ## NMDS Plot
+# to be able to see what sites and plots points belong to 
+data.scoresb <- data.scoresb %>% 
+                  mutate(site_nr2 = as.numeric(site_nr)) %>% 
+                  mutate(plot_rep2 = case_when(plot_rep == "1" ~ "A",
+                                               plot_rep == "2" ~ "B",
+                                               plot_rep == "3" ~ "C")) %>% 
+                  mutate(site_plot = str_c(site_nr, "", plot_rep2))
+
+
 (nmds_plot3 <- ggplot(data.scoresb, aes(x = NMDS1, y = NMDS2)) + 
                   geom_polygon(data = df_ell2, aes(x = NMDS1, y = NMDS2, group = group,
                                                    color = group, fill = group), alpha = 0.2, 
@@ -601,14 +630,25 @@ gisarrows2 <- gisarrows2[!(row.names(gisarrows2) %in% remove),]
                                     labels = c("N", "S"),
                                     name = "Aspect"))
 
-# without aspect
-data.scoresb <- data.scoresb %>% 
-                  mutate(site_nr2 = as.numeric(site_nr)) %>% 
-                  mutate(plot_rep2 = case_when(plot_rep == "1" ~ "A",
-                                               plot_rep == "2" ~ "B",
-                                               plot_rep == "3" ~ "C")) %>% 
-                  mutate(site_plot = str_c(site_nr, "", plot_rep2))
+# with plot and site names as points 
+(nmds_plot3b <- ggplot(data.scoresb, aes(x = NMDS1, y = NMDS2)) + 
+                  geom_polygon(data = df_ell2, aes(x = NMDS1, y = NMDS2, group = group,
+                                                   fill = group),
+                               size = 0.5, linetype = 1, alpha = 0.2) +
+                  geom_text(aes(label = site_plot, color = site_nr2), size = 3) +
+                  geom_segment(aes(x = 0, y = 0, xend = NMDS1, yend = NMDS2), 
+                               data = gisarrows2, size = 1, alpha = 0.7, colour = "grey40",
+                               arrow = arrow(length = unit(0.25, "cm"))) +
+                  geom_text(data = gisarrows2, aes(x = NMDS1, y = NMDS2), colour = "grey40", 
+                            label = row.names(gisarrows2)) + 
+                  theme_thesis + 
+                  labs(x = "NMDS1", y = "NMDS2") +
+                  scale_color_viridis_c(option = "plasma") +
+                  scale_fill_manual(values = c("#458264", "#F4A460"),
+                                    labels = c("N", "S"),
+                                    name = "Aspect"))
 
+# without aspect
 (nmds_plot4 <- ggplot(data.scoresb, aes(x = NMDS1, y = NMDS2)) + 
                   geom_text(aes(label = site_plot, color = site_nr2), size = 3) +
                   #   geom_text(data = species_scores, aes(x = NMDS1, y = NMDS2, label = species),
